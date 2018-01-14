90154f7810e5e437a6b740744baa470754bcca3d
YOUR NAME <YOUR@EMAIL.com>
2018-01-14 21:38:22 +0700

    ruby1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :books, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
